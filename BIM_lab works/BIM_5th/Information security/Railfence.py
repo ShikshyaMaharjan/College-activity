3972fccaea2ebc593b774068f9c31387191c4807
Shikshya Maharjan <maharjanshikshya7@gmail.com>
2026-02-24 21:57:26 +0545

    BIM_5th labwork

--- BIM_lab works/BIM_5th/Information security/Railfence.py
+++ BIM_lab works/BIM_5th/Information security/Railfence.py
@@ -1,41 +1,82 @@
-# Rail Fence Cipher 
+# Rail Fence Cipher Correct Version
+
 def encrypt(message, key):
+
     rails = ['' for _ in range(key)]
-    row, step = 0, 1
+
+    row = 0
+    step = 1
+
     for char in message:
+
         rails[row] += char
+
+        # change direction BEFORE updating row
+        if row == 0:
+            step = 1
+        elif row == key - 1:
+            step = -1
+
         row += step
-        if row == 0 or row == key - 1:
-            step *= -1
 
     return ''.join(rails)
+
+
 def decrypt(cipher, key):
+
     n = len(cipher)
+
     zigzag = []
-    row, step = 0, 1
-    for _ in range(n):
+
+    row = 0
+    step = 1
+
+    for i in range(n):
+
         zigzag.append(row)
+
+        if row == 0:
+            step = 1
+        elif row == key - 1:
+            step = -1
+
         row += step
-        if row == 0 or row == key - 1:
-            step *= -1
-    # Step 2: fill rails
+
+
     rails = ['' for _ in range(key)]
+
     index = 0
+
     for r in range(key):
         for i in range(n):
             if zigzag[i] == r:
                 rails[r] += cipher[index]
                 index += 1
-    # Step 3: rebuild message
+
+
     result = ''
+
     counters = [0] * key
+
     for r in zigzag:
+
         result += rails[r][counters[r]]
+
         counters[r] += 1
+
+
     return result
+
+
+# Main
+
 message = input("Enter message: ")
-key = int(input("Enter number of rails: "))
+key = int(input("Enter key: "))
+
 cipher = encrypt(message, key)
+
 print("Encrypted:", cipher)
+
 plain = decrypt(cipher, key)
+
 print("Decrypted:", plain)
